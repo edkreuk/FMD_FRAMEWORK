108ca7f0a9afddf6233d230b93da140540cc18d2
Erwin de Kreuk <erwinde.kreuk@insparklabs.nl>
2025-11-13 14:13:06 +0100

    support for custom notebooks

--- src/NB_FMD_LOAD_BRONZE_SILVER.Notebook/notebook-content.py
+++ src/NB_FMD_LOAD_BRONZE_SILVER.Notebook/notebook-content.py
@@ -107,70 +107,7 @@ token =  notebookutils.credentials.getToken('https://analysis.windows.net/powerb
 
 # CELL ********************
 
-def build_exec_statement(proc_name, **params):
-    param_strs = []
-    for key, value in params.items():
-        if value is not None:
-            if isinstance(value, str):
-                param_strs.append(f"@{key}='{value}'")
-            else:
-                param_strs.append(f"@{key}={value}")
-    
-    if param_strs:
-        return f"EXEC {proc_name}, " + ", ".join(param_strs)
-    else:
-        return f"EXEC {proc_name}"
-
-
-# METADATA ********************
-
-# META {
-# META   "language": "python",
-# META   "language_group": "synapse_pyspark"
-# META }
-
-# CELL ********************
-
-def execute_with_logging(exec_statement, driver, connstring, database, **params):
-
-    # Get token for Azure SQL authentication
-    token = notebookutils.credentials.getToken('https://analysis.windows.net/powerbi/api').encode("UTF-16-LE")
-    token_struct = struct.pack(f'<I{len(token)}s', len(token), token)
-
-    # Build connection
-    conn = pyodbc.connect(
-        f"DRIVER={driver};SERVER={connstring};PORT=1433;DATABASE={database};",
-        attrs_before={1256: token_struct},
-        timeout=12
-    )
-
-    exec_statement = build_exec_statement(exec_statement, **params)
-
-    start_time = datetime.datetime.utcnow()
-    status = "Success"
-    error_message = None
-
-    try:
-        with conn.cursor() as cursor:
-            # Warm-up query
-            cursor.execute("SELECT 1")
-            cursor.fetchone()
-            conn.timeout = 10
-
-
-            # Build EXEC statement dynamically
-
-            print(f"Executing: {exec_statement}")
-
-            cursor.execute(exec_statement)
-            cursor.commit()
-
-    except pyodbc.OperationalError as e:
-        print(e) 
-    except Exception as e:
-        status = "Failed"
-        error_message = str(e)
-        print(f"Error: {error_message}")
+%run NB_FMD_UTILITY_FUNCTIONS
 
 # METADATA ********************
 
@@ -267,9 +204,6 @@ spark.conf.set("spark.sql.parquet.datetimeRebaseModeInWrite", "CORRECTED")
 spark.conf.set('spark.microsoft.delta.optimize.fast.enabled', True)
 spark.conf.set('spark.microsoft.delta.optimize.fileLevelTarget.enabled', True)
 spark.conf.set('spark.databricks.delta.autoCompact.enabled', True)
-
-#This is required to enable optimal refreshed for materialized lake views
-spark.conf.set('spark.databricks.delta.properties.defaults.enableChangeDataFeed', True)
 
 # METADATA ********************
 
---
+++ src/NB_FMD_PROCESSING_LANDINGZONE_MAIN.Notebook/notebook-content.py
@@ -0,0 +1,232 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse_name": "",
+# META       "default_lakehouse_workspace_id": "",
+# META       "known_lakehouses": []
+# META     },
+# META     "environment": {}
+# META   }
+# META }
+
+# MARKDOWN ********************
+
+# # Parameters
+
+# CELL ********************
+
+import uuid
+import requests
+
+from datetime import datetime
+from json import loads, dumps
+from py4j.protocol import Py4JJavaError
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# PARAMETERS CELL ********************
+
+###############################Target Parameters###############################
+EntityId = 45
+EntityLayer = "LandingZone"
+DataSourceName = "nb"
+TargetFilePath = "fmd/NB/2025/11/13"
+TargetFileName = "sample_202511130933.parquet"
+TargetLakehouseGuid = "01b832e3-2746-4e9c-b169-540b2741a638"
+WorkspaceGuid = "40e27fdc-775a-4ee2-84d5-48893c92d7cc"
+LastLoadValue = None
+CustomNotebookName = "NB_FMD_CUSTOM_NOTEBOOK_TEMPLATE"
+TriggerGuid = ""
+TriggerTime = ""
+TriggerType = ""
+
+###############################Logging Parameters###############################
+driver = '{ODBC Driver 18 for SQL Server}'
+connstring='nl7yhqnbrscude3yv6mas6bxpq-tndhi54vbs4urpccunwiydulzi.database.fabric.microsoft.com'
+database='SQL_FMD_FRAMEWORK-075e5656-613d-41f4-89ed-63f6837c0ff6'
+result_data=''
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+def format_guid(input_str: str) -> str:
+    """
+    Formats the input string by adding hyphens at specific positions if they are missing.
+    Parameters:
+    - input_str (str): The input string to be formatted.
+    Returns:
+    - str: The formatted string.
+    """
+    if "-" not in input_str and len(input_str) == 32:
+        formatted_str = '-'.join([input_str[:8], input_str[8:12], input_str[12:16], input_str[16:20], input_str[20:]])
+        return formatted_str
+    else:
+        return input_str
+def is_valid_guid(guid_str: str) -> bool:
+    """
+    Check if a string is a valid GUID.
+
+    Parameters:
+    - guid_str (str): The string to be checked.
+
+    Returns:
+    - bool: True if the string is a valid GUID, False otherwise.
+    """
+    try:
+        uuid_obj = uuid.UUID(guid_str)
+        # Check if the UUID is valid
+        return str(uuid_obj) == guid_str
+    except ValueError:
+        return False
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+## always use notebook guid instead of pipeline id
+PipelineName = notebookutils.runtime.context.get('currentNotebookName')
+PipelineGuid = str(notebookutils.runtime.context.get('currentNotebookId'))
+WorkspaceGuid = notebookutils.runtime.context.get('currentWorkspaceId')
+PipelineParentRunGuid = notebookutils.runtime.context.get('PipelineParentRunGuid')
+PipelineRunGuid = str(uuid.uuid4())
+TriggerGuid = format_guid(TriggerGuid)
+NotebookExecutionId = str(uuid.uuid4())
+
+if not PipelineParentRunGuid:
+    PipelineParentRunGuid='00000000-0000-0000-0000-000000000000'
+if not PipelineGuid:
+    PipelineGuid='00000000-0000-0000-0000-000000000000'
+if not TriggerGuid or not is_valid_guid(TriggerGuid):
+    TriggerGuid = '00000000-0000-0000-0000-000000000000'
+if not TriggerTime:
+    TriggerTime=''
+if not TriggerType:
+    TriggerType=''
+if not WorkspaceGuid:
+    WorkspaceGuid='00000000-0000-0000-0000-000000000000'
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+starttime = datetime.now()
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# MARKDOWN ********************
+
+# ## Define Notebooks settings
+
+# CELL ********************
+
+notebook_params = {}
+notebook_params["PipelineGuid"] = PipelineGuid
+notebook_params["PipelineName"] = PipelineName
+notebook_params["TriggerGuid"] = TriggerGuid
+notebook_params["TriggerType"] = TriggerType
+notebook_params["TriggerTime"] = TriggerTime
+notebook_params["WorkspaceGuid"] = WorkspaceGuid  
+notebook_params["PipelineParentRunGuid"] = PipelineParentRunGuid
+notebook_params["PipelineRunGuid"] = PipelineRunGuid
+notebook_params["NotebookExecutionId"] = NotebookExecutionId
+notebook_params["driver"] = driver
+notebook_params["connstring"] = connstring
+notebook_params["database"] = database
+notebook_params["EntityId"] = EntityId 
+notebook_params["EntityLayer"] = EntityLayer 
+notebook_params["DataSourceName"] = DataSourceName 
+notebook_params["TargetFilePath"] = TargetFilePath 
+notebook_params["TargetFileName"] = TargetFileName 
+notebook_params["TargetLakehouseGuid"] = TargetLakehouseGuid 
+notebook_params["WorkspaceGuid"] = WorkspaceGuid 
+notebook_params["LastLoadValue"] = LastLoadValue 
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# MARKDOWN ********************
+
+# ## Execute Notebook
+
+# CELL ********************
+
+result = None
+fail = None
+try:
+    result = notebookutils.notebook.run(CustomNotebookName, 900, notebook_params)
+except Py4JJavaError as e:
+    # Inspect the Java exception message
+    if "NotebookExecutionException" in str(e):
+        fail = e
+except Exception as e:
+    print(notebook_params)
+    raise(e)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+if fail:
+    raise ValueError(F"""Notebook: {CustomNotebookName} failed with error: {e}""")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+TotalRuntime = str((datetime.now() - starttime))
+notebookutils.mssparkutils.notebook.exit(result)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
