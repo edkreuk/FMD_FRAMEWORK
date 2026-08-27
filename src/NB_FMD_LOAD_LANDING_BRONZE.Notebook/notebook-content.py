49a29188d7cf633f9f6402b3aff67605896f25a4
Erwin de Kreuk <erwin.dekreuk@gmail.com>
2026-08-27 17:55:07 +0200

    NEE Enabled

--- src/NB_FMD_LOAD_LANDING_BRONZE.Notebook/notebook-content.py
+++ src/NB_FMD_LOAD_LANDING_BRONZE.Notebook/notebook-content.py
@@ -113,8 +113,8 @@ import re
 from datetime import datetime, timezone
 import json
 from delta.tables import *
-from pyspark.sql.functions import sha2, md5, concat_ws, current_timestamp
-from pyspark.sql.types import StringType
+from pyspark.sql.functions import sha2, md5, concat_ws, current_timestamp, col
+from pyspark.sql.types import StringType, ByteType, ShortType
 
 # METADATA ********************
 
@@ -330,10 +330,21 @@ elif SourceFileType=='xls':
 
 else:
     #Read all incoming changes in Parquet format
-    dfDataChanged= spark.read\
-                    .format(SourceFileType) \
-                    .option("header","true") \
-                    .load(f"{source_changes_data_path}")
+    #NEE expects int instead of Tinyint
+    converted_path = convert_small_numeric_columns_to_int(source_changes_data_path)
+
+    if converted_path:
+        print(f"Use converted file: {converted_path}")
+        source_changes_data_path=converted_path
+        dfDataChanged= spark.read\
+                        .format(SourceFileType) \
+                        .option("header","true") \
+                        .load(f"{source_changes_data_path}")
+    else:
+        dfDataChanged= spark.read\
+                        .format(SourceFileType) \
+                        .option("header","true") \
+                        .load(f"{source_changes_data_path}")
 
 
 # METADATA ********************
--- src/NB_FMD_UTILITY_FUNCTIONS.Notebook/notebook-content.py
+++ src/NB_FMD_UTILITY_FUNCTIONS.Notebook/notebook-content.py
@@ -16,8 +16,80 @@
 
 # CELL ********************
 
-import struct, pyodbc
+import struct, pyodbc, os
+from pyspark.sql.functions import col
+from pyspark.sql.types import ByteType, ShortType
 
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+def convert_small_numeric_columns_to_int(path):
+
+    # Read source file
+    df = (
+        spark.read
+            .format(SourceFileType)
+            .load(path)
+    )
+
+    # Find ByteType and ShortType columns
+    changed_columns = [
+        (f.name, f.dataType.simpleString())
+        for f in df.schema.fields
+        if isinstance(f.dataType, (ByteType, ShortType))
+    ]
+
+    # Nothing to convert
+    if not changed_columns:
+        print(f"No ByteType or ShortType columns found in {path}")
+        return None
+
+    print(
+        f"Converting {len(changed_columns)} columns from ByteType/ShortType to IntegerType:"
+    )
+
+    for name, dtype in changed_columns:
+        print(f" - {name}: {dtype} -> integer")
+
+    # Cast columns
+    df = df.select(
+        *[
+            col(f.name).cast("integer").alias(f.name)
+            if isinstance(f.dataType, (ByteType, ShortType))
+            else col(f.name)
+            for f in df.schema.fields
+        ]
+    )
+
+    file_name, ext = os.path.splitext(SourceFileName)
+
+    converted_path = (
+        f"abfss://{SourceWorkspace}"
+        f"@onelake.dfs.fabric.microsoft.com/"
+        f"{SourceLakehouse}/Files/"
+        f"{SourceFilePath}/"
+        f"{file_name}_converted{ext}"
+    )
+
+    spark.conf.set("spark.native.enabled", "false")
+    # Write parquet
+    (
+        df.write
+          .mode("overwrite")
+          .parquet(converted_path)
+    )
+    spark.conf.set("spark.native.enabled", "true")
+
+    print(f"Converted parquet written to: {converted_path}")
+
+    return converted_path
 
 # METADATA ********************
 
